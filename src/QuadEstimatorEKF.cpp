3fb0c8d7ba3443472c7e053acf47a423b6d1aec7
yd wu <yiidtw@gmail.com>
2021-03-21 21:45:49 +0800

    pass scenario 11

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -302,6 +302,19 @@ void QuadEstimatorEKF::UpdateFromGPS(V3F pos, V3F vel)
   //  - this is a very simple update
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
 
+  zFromX(0) = ekfState(0);
+  zFromX(1) = ekfState(1);
+  zFromX(2) = ekfState(2);
+  zFromX(3) = ekfState(3);
+  zFromX(4) = ekfState(4);
+  zFromX(5) = ekfState(5);
+
+  hPrime(0, 0) = 1.f;
+  hPrime(1, 1) = 1.f;
+  hPrime(2, 2) = 1.f;
+  hPrime(3, 3) = 1.f;
+  hPrime(4, 4) = 1.f;
+  hPrime(5, 5) = 1.f;
 
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
